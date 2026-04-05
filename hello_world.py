4c22fa93b8b6c51cf90f8a6203761e4e9b304158
shashiojha8-creator <shashiojha8@gmail.com>
2026-04-05 09:38:57 +0530

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+# --- My First Professional Logic ---
+import datetime
+
+def start_journey():
+    current_time = datetime.datetime.now()
+    print(f"Bokaro Logic-Lab active at: {current_time}")
+    print("Market observation mode: ON")
+
+if __name__ == "__main__":
+    start_journey()
